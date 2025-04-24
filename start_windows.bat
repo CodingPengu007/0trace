f913bf24e022ef69e650a3b4aca5436ff13c20e3
CodingPengu007 <pengu@sky-network.org>
2025-04-24 11:28:52 +0200

    fixed bug

--- start_windows.bat
+++ start_windows.bat
@@ -1,28 +1,14 @@
 @echo off
 
-:: Function to print error messages and exit
-:ErrorExit
-echo %1 >&2
-exit /b 1
-
 :: Create and activate virtual environment if it doesn't exist
 if not exist "Otrace_venv" (
-    python -m venv Otrace_venv || call :ErrorExit "Failed to create virtual environment."
+    python -m venv Otrace_venv
 )
 
 :: Activate the virtual environment
-call Otrace_venv\Scripts\activate.bat || call :ErrorExit "Failed to activate virtual environment."
+call Otrace_venv\Scripts\activate.bat
 
 :: Install dependencies
-python -m pip install --upgrade pip || call :ErrorExit "Failed to upgrade pip."
-pip install -r requirements.txt || call :ErrorExit "Failed to install required packages."
-
-:: Upgrade outdated packages if any
-for /f "delims=" %%i in ('python -m pip list --outdated') do (
-    for /f "tokens=1 delims==" %%j in ("%%i") do (
-        echo Upgrading %%j
-        python -m pip install --upgrade %%j || call :ErrorExit "Failed to upgrade some packages."
-    )
-)
+pip install -r requirements.txt
 
 echo Setup completed successfully.
