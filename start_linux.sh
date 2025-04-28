0b35dee1e54c9166c5dfcce9b4331e3763f3f268
CodingPengu007 <christoph@seyferts.net>
2025-04-28 19:57:43 +0200

    Sped up startup scripts

--- start_linux.sh
+++ start_linux.sh
@@ -2,19 +2,9 @@
 if [ ! -d "Otrace_venv" ]; then
     python3 -m venv Otrace_venv
 
-<<<<<<< HEAD
     source Otrace_venv/bin/activate
     pip install --upgrade pip
 
-=======
-    # Activate the virtual environment
-    source Otrace_venv/bin/activate
-
-    # Install dependencies
-    pip install --upgrade pip
-
-    # Install required packages
->>>>>>> c470e6e651d04d481fcd7b060a6e16d7ec3c5d8f
     required_packages=(
         bcrypt
         textual==0.89.1
@@ -31,17 +21,10 @@ if [ ! -d "Otrace_venv" ]; then
         fi
         pip install "$package"
     done
-<<<<<<< HEAD
-
-=======
->>>>>>> c470e6e651d04d481fcd7b060a6e16d7ec3c5d8f
 fi
 
 source Otrace_venv/bin/activate
-<<<<<<< HEAD
 pip install --upgrade pip
-=======
->>>>>>> c470e6e651d04d481fcd7b060a6e16d7ec3c5d8f
 
 # Upgrade outdated packages except textual and textual_textarea
 outdated_packages=$(pip list --outdated --format=columns | awk 'NR>2 {print $1}' | grep -Ev '^(textual|textual_textarea)$')
--- start_macos.sh
+++ start_macos.sh
@@ -2,19 +2,9 @@
 if [ ! -d "Otrace_venv" ]; then
     python3 -m venv Otrace_venv
 
-<<<<<<< HEAD
     source Otrace_venv/bin/activate
     pip install --upgrade pip
 
-=======
-    # Activate the virtual environment
-    source Otrace_venv/bin/activate
-
-    # Install dependencies
-    pip install --upgrade pip
-
-    # Install required packages
->>>>>>> c470e6e651d04d481fcd7b060a6e16d7ec3c5d8f
     required_packages=(
         bcrypt
         textual==0.89.1
@@ -31,17 +21,11 @@ if [ ! -d "Otrace_venv" ]; then
         fi
         pip install "$package"
     done
-<<<<<<< HEAD
-
 fi
 
-=======
-fi
-
-# Activate the virtual environment
 source Otrace_venv/bin/activate
+pip install --upgrade pip
 
->>>>>>> c470e6e651d04d481fcd7b060a6e16d7ec3c5d8f
 # Upgrade outdated packages except textual and textual_textarea
 outdated_packages=$(pip list --outdated --format=columns | awk 'NR>2 {print $1}' | grep -Ev '^(textual|textual_textarea)$')
 if [ -n "$outdated_packages" ]; then
@@ -49,10 +33,4 @@ if [ -n "$outdated_packages" ]; then
     echo "$outdated_packages" | xargs -n1 pip install --upgrade
 else
     echo "All packages are up to date (excluding textual and textual_textarea)."
-<<<<<<< HEAD
 fi
-=======
-fi
-
-echo "Setup completed successfully."
->>>>>>> c470e6e651d04d481fcd7b060a6e16d7ec3c5d8f
